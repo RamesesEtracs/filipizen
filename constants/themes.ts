98dc3c831e85d589276b526803e4c4ac658f1a80
Clent <bacatan.ramesessystems@gmail.com>
2023-10-27 08:55:22 +0800

    structures the folder

---
+++ constants/themes.ts
@@ -0,0 +1,26 @@
+const color = {
+    
+    primary: "#F2BB4E",
+    secondary: "#FFD889",
+    tertiary: "#FFECC9",
+
+    dirtyWhite: "#FFEFCE",
+    fadedWhite: "#FFF8EB",
+}
+
+const textColor = {
+
+    black: "#222222",
+    lightBlack: "#3D3D3D",
+    fadedGrey: "#BBBBBB",
+    white: "#FFFFFF"
+}
+
+const font = {
+    
+    light: "RobotoLight",
+    regular: "RobotoRegular",
+    medium: "RobotoMedium",
+    bold: "RobotoBold",
+}
+
